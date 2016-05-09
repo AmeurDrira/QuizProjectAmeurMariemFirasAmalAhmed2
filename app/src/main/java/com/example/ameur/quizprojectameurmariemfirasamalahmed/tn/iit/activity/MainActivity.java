407b113bfc0c8067bf004802cf37b463946d8927
ameur92 <ameur.drira@gmail.com>
2016-05-09 18:44:42 +0100

    « StructurePackage »

--- app/src/main/java/com/example/ameur/quizprojectameurmariemfirasamalahmed/tn/iit/activity/MainActivity.java
+++ app/src/main/java/com/example/ameur/quizprojectameurmariemfirasamalahmed/tn/iit/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ameur.quizprojectameurmariemfirasamalahmed;
+package com.example.ameur.quizprojectameurmariemfirasamalahmed.tn.iit.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ameur.quizprojectameurmariemfirasamalahmed.R;
 
 public class MainActivity extends AppCompatActivity {
 
